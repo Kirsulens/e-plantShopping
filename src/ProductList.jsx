5befc218708bb0ffd760794df39e6dbebc5d79e4
Kirsulens <rencene2000@gmail.com>
2024-12-29 22:22:34 -0500

    trying to get plant pictures

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -268,10 +268,14 @@ const handlePlantsClick = (e) => {
         </div>
         {!showCart? (
         <div className="product-grid">
-            {plantsArray.map((plant, index) => (
-            <div className="product-card" key={index}>
-            <img className="product-image" src={plant.image} alt={plant.name} />
-            <div className="product-title">{plant.name}</div>
+            {plantsArray.map((category, index) => (
+            <div key={index}>
+            <h1><div>{category.category}</div></h1>
+            <div className="product-list">
+                {category.plants.map((plant, plantIndex) => (
+                <div className="product-card" key={plantIndex}>
+                <img className="product-image" src={plant.image} alt={plant.name} />
+                <div className="product-title">{plant.name}</div>
             <div className="product-description">{plant.description}</div>
             <div className="product-cost">{plant.cost}</div>
             <button className="product-button">Add to Cart</button>
