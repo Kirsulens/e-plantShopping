0acc6346370ebac1f4eb062b97a7b5ed9c0ffa2a
Kirsulens <rencene2000@gmail.com>
2024-12-29 23:10:41 -0500

    handleAddToCart

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,11 +1,21 @@
 import React, { useState,useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
-import addItem from './CartSlice';
+import { useDispatch } from 'react-redux';
+import {addItem} from './CartSlice';
 function ProductList() {
     const [showCart, setShowCart] = useState(false); 
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
-    const [addedToCart, setAddedToCart] = useState({});
+    const ProductList = () => {
+        const dispatch = useDispatch();
+        const [addedToCart, setAddedToCart] = useState({});    
+    const handleAddToCart = (product) => {
+        dispatch(addItem(product));
+        setAddedToCart((prevState) => ({
+           ...prevState,
+           [product.name]: true, // Set the product name as key and value as true to indicate it's added to cart
+         }));
+      };
     const plantsArray = [
         {
             category: "Air Purifying Plants",
@@ -247,13 +257,6 @@ const handlePlantsClick = (e) => {
     e.preventDefault();
     setShowCart(false);
   };
-  const handleAddToCart = (product) => {
-    dispatch(addItem(product));
-    setAddedToCart((prevState) => ({
-       ...prevState,
-       [product.name]: true, // Set the product name as key and value as true to indicate it's added to cart
-     }));
-  };
     return (
         <div>
             <div className="navbar" style={styleObj}>
@@ -298,5 +301,5 @@ const handlePlantsClick = (e) => {
         </div>
     );
 }
-
+}
 export default ProductList;
